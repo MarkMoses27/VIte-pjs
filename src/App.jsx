8fd975243689030f237d4dba689dc2301be0b487
MarkMoses27 <markmoses817@gmail.com>
2023-09-22 13:25:12 +0300

    3 files modified, 6 added, 1 deleted

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from './NavBar';
+import Home from './Home';
+import About from './About';
+import Projects from './Projects';
+import Contact from './Contact';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/contact" component={Contact} />
+      </Switch>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
